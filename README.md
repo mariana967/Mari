f580721fd915bf178eac9144e3666d3742f71049
mariana967 <82124463+mariana967@users.noreply.github.com>
2021-04-13 20:54:57 -0500

    Mari

---
+++ README.md
@@ -0,0 +1,3 @@
+# PWA_IMF_51M
+Desarrollado por Cristian Garcia Alvarado.
+ Ejemplo con pwa 
---
+++ js/init.js
@@ -0,0 +1,11 @@
+// @ts-ignore
+firebase.initializeApp({
+    
+
+    apiKey: "AIzaSyCisnOh62j-SMPBPxSatJrdCCzwSaejb7E",
+    authDomain: "pwamp-9f8ee.firebaseapp.com",
+    projectId: "pwamp-9f8ee",
+    storageBucket: "pwamp-9f8ee.appspot.com",
+    messagingSenderId: "1033309295511",
+    appId: "1:1033309295511:web:68a5aecc33d93485166ad2"
+});
